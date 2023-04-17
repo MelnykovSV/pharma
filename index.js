74448d6021d6601a0c2b69b28fc0ea73c644d1f0
MelnykovSV <svmelnykov@gmail.com>
2023-04-17 03:57:44 +0300

    cleared a bit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// import "./nice-select2/js/nice-select2.js";
-import "./js-new/custom-selects.js";
-import "./js-new/price-range-slider.js";
+import './js/custom-selects.js';
+import './js/price-range-slider.js';
 
-import "./js-new/tabs.js";
-import "./js-new/slider-offers.js";
-import "./js-new/slider-info.js";
-// import "./js-new/range.js";
+import './js/tabs.js';
+import './js/slider-offers.js';
+import './js/slider-info.js';
