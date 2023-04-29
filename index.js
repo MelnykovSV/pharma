7e02e1fb2adb5f1885418101c4f5a0e631f5870b
MelnykovSV <svmelnykov@gmail.com>
2023-04-29 19:59:51 +0300

    nice-select2 test

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-import './js/custom-selects.js';
+// import './js/custom-selects.js';
+import './test-select.js';
 import './js/price-range-slider.js';
 import './js/accordion.js';
 import './js/tabs.js';
-// import './pagination/pagination-system.umd.min.js';
 
-// import './js/catalogheightHandler.js';
 import './js/cardsZIndexHandler.js';
 import './js/pagination.js';
-// import './js/slider-offers.js';
-// import './js/slider-info.js';
