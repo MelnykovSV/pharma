0dc535282f2a8f298182d249e8893bdd7e0d40f0
MelnykovSV <svmelnykov@gmail.com>
2023-10-26 01:14:32 +0300

    fixed some problems with outlines on focus

--- js/expandSearchBar.js
+++ js/expandSearchBar.js
@@ -1,12 +1,15 @@
 const button = document.querySelector('.searchbar-button');
 const searchbarContainer = document.querySelector('.searchbar-container');
 const searchbarListItem = document.querySelector('.searchbar-list-item');
+const searchbarInput = document.querySelector('.searchbar-input');
 
 const closeButton = document.querySelector('.searchbar-close-button');
 
 function searchBarOpenHandler() {
   searchbarContainer.classList.add('searchbar-expanded');
   searchbarListItem.classList.add('searchbar-expanded');
+  closeButton.setAttribute('tabIndex', '0');
+  searchbarInput.setAttribute('tabIndex', '0');
 
   searchbarContainer.style.zIndex = 5;
 }
@@ -14,6 +17,8 @@ function searchBarOpenHandler() {
 function searchBarCloseHandler() {
   searchbarContainer.classList.remove('searchbar-expanded');
   searchbarListItem.classList.remove('searchbar-expanded');
+  closeButton.setAttribute('tabIndex', '-1');
+  searchbarInput.setAttribute('tabIndex', '-1');
 
   setTimeout(() => {
     searchbarContainer.style.zIndex = 0;
