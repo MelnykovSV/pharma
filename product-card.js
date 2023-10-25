2556905ae285306fe58e68035afad2014b5a57e3
MelnykovSV <svmelnykov@gmail.com>
2023-10-25 23:43:33 +0300

    added modified header and script to product-card page

--- product-card.js
+++ product-card.js
@@ -1,6 +1,6 @@
-// import './js/custom-selects.js';
-// import './js/price-range-slider.js';
+
 import './js/accordion.js';
 import './js/tabs.js';
 import './js/slider-offers.js';
 import './js/slider-info.js';
+import './js/expandSearchBar.js';
